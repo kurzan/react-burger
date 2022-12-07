8f43ee3f23b98c15aea0b2f3d4e88dd6cda53453
Kurzov <kurzanbl4@gmail.com>
2022-12-07 12:42:51 +0300

    Реализация Modal через портал

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useCallback, useState} from "react";
+import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from "./modal.module.css";
 import ModalOverlay from '../modal-overlay/modal-overlay';
+
+const modalRoot = document.querySelector('#modals');
 
 
 const Modal = ({children, title, onClose}) => {
@@ -33,7 +36,7 @@ const Modal = ({children, title, onClose}) => {
     setIsHovered(!isHovered);
   }
 
-  return (
+  return ReactDOM.createPortal(
     <>
     {isOpen &&
       <>
@@ -49,7 +52,8 @@ const Modal = ({children, title, onClose}) => {
         </div>
       </>
     }
-    </>
+    </>,
+    modalRoot
   )
 };
 
