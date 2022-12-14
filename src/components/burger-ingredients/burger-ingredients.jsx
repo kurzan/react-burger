e43b7161c3fab69d7ff0c452ea30c9bef9f195da
Kurzov <kurzanbl4@gmail.com>
2022-12-14 13:35:59 +0300

    Скролл с помощью inView

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,43 +1,20 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import dataPropTypes from '../../utils/types.js'
 import { Tab, CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-ingredients.module.css';
+import { useInView } from 'react-intersection-observer';
 
 
-const tabs = [{id: 'bun', title: 'Булки'}, {id: 'sauce', title: 'Соусы'}, {id: 'main', title: 'Начинки'}];
-
-const Tabs = () => {
-  
-  const [current, setCurrent] = useState('bun');
-
-  useEffect(() => {
-    const target = document.querySelector(`#${current}`);
-    target.scrollIntoView();
-  }, [current]);
-  
-  return (
-    
-    <div className="mt-5 mb-10" style={{ display: 'flex' }}>
-        {tabs.map(tabItem => (
-          <Tab key={tabItem.id} value={tabItem.id} active={current === tabItem.id} onClick={setCurrent}>
-            {tabItem.title}
-          </Tab>
-        ))}
-    </div>
-  )
-}
-
-const MenuElement = ({ingredient, onIngredientClick}) => {
+const MenuElement = ({ ingredient, onIngredientClick }) => {
 
   const onClick = () => {
     onIngredientClick(ingredient)
   }
 
-
   return (
     <div className={styles.menu_element} onClick={onClick} >
-      <span className={styles.counter} ><Counter /></span>  
+      <span className={styles.counter} ><Counter /></span>
       <img className="ml-4 mr-4 mb-1" src={ingredient.image} alt={ingredient.name} />
       <div className={styles.price_wrapper}>
         <p className="text text_type_digits-default">{ingredient.price}</p>
@@ -49,59 +26,93 @@ const MenuElement = ({ingredient, onIngredientClick}) => {
   )
 }
 
-MenuElement.propTypes = { 
+MenuElement.propTypes = {
   ingredient: PropTypes.shape(
-      dataPropTypes.isRequired).isRequired,
+    dataPropTypes.isRequired).isRequired,
   onIngredientClick: PropTypes.func.isRequired
 };
 
-const Menu = ({data, onIngredientClick}) => {
 
+const BurgerIngredients = ({ data, onIngredientClick }) => {
+  const tabs = [{ id: 'bun', title: 'Булки' }, { id: 'sauce', title: 'Соусы' }, { id: 'main', title: 'Начинки' }];
 
-  return (
-    <>
-      { tabs.map((item, i) => {
-        const id = item.id;
-        
-          return (
-            <div className={styles.menu} key={i}>   
-              <p className="text text_type_main-medium" id={item.id}>{item.title}</p>
-              <ul className={styles.menu_wrapper}>
-                {data.map((item, i) => {
-                  if(item.type === id) {
-                  return <MenuElement key={i} ingredient={item} onIngredientClick={onIngredientClick}/>
-                  }
+  const [current, setCurrent] = useState('bun');
+  
+  const { ref: bunRef, inView: inBunView } = useInView();
+  const { ref: sauceRef, inView: inSauceView } = useInView();
+  const { ref: mainRef, inView: inMainView } = useInView();
 
-                  return null;
-                  })}
-              </ul>
-            </div>  
-          )
-        })}
-    </>
-  )
-}
+  useEffect(() => {
+    const target = document.querySelector(`#${current}`);
+    target.scrollIntoView();
 
-Menu.propTypes = { 
-  data: PropTypes.arrayOf(
-    PropTypes.shape(
-      dataPropTypes.isRequired).isRequired),
-  onIngredientClick: PropTypes.func.isRequired
-};
+  }, [current]);
 
-const BurgerIngredients = ({data, onIngredientClick}) => {
+  useEffect(() => {
+    if (inBunView) {
+      setCurrent('bun')
+    } else if (inSauceView) {
+      setCurrent('sauce')
+    } else if (inMainView) {
+      setCurrent('main')
+    }
+  }, [inBunView, inSauceView, inMainView])
+
   return (
     <section className={styles.build_burger}>
       <h1 className="mt-10 text text_type_main-large">Соберите бургер</h1>
-      <Tabs />
-      <div className={styles.menu_container}>
-        <Menu data={data} onIngredientClick={onIngredientClick}/>
+
+      <div className="mt-5 mb-10" style={{ display: 'flex' }}>
+        {tabs.map(tabItem => (
+          <Tab key={tabItem.id} value={tabItem.id} active={current === tabItem.id} onClick={setCurrent}>
+            {tabItem.title}
+          </Tab>
+        ))}
       </div>
+      <div className={styles.menu_container}>
+
+        <div className={styles.menu} ref={bunRef}>
+          <p className="text text_type_main-medium" id='bun'>Булки</p>
+          <ul className={styles.menu_wrapper}>
+            {data.map((item, i) => {
+              if (item.type === 'bun') {
+                return <MenuElement key={i} ingredient={item} onIngredientClick={onIngredientClick} />
+              }
+              return null;
+            })}
+          </ul>
+        </div>
+
+        <div className={styles.menu} ref={sauceRef}>
+          <p className="text text_type_main-medium" id='sauce'>Соусы</p>
+          <ul className={styles.menu_wrapper}>
+            {data.map((item, i) => {
+              if (item.type === 'sauce') {
+                return <MenuElement key={i} ingredient={item} onIngredientClick={onIngredientClick} />
+              }
+              return null;
+            })}
+          </ul>
+        </div>
+
+        <div className={styles.menu} ref={mainRef}>
+          <p className="text text_type_main-medium" id='main'>Начинки</p>
+          <ul className={styles.menu_wrapper}>
+            {data.map((item, i) => {
+              if (item.type === 'main') {
+                return <MenuElement key={i} ingredient={item} onIngredientClick={onIngredientClick} />
+              }
+              return null;
+            })}
+          </ul>
+        </div>
+      </div>
+
     </section>
   )
 }
 
-BurgerIngredients.propTypes = { 
+BurgerIngredients.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape(
       dataPropTypes.isRequired).isRequired),
